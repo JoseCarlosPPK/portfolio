ce1cb4f7921039dbad3be48aaa0c8545c687b91e
JoseCarlos <josecarlosxd27@gmail.com>
2026-01-12 19:03:39 +0100

    Add ROUTES variable and use it in router

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+   ABOUT_ME: '/',
+   PROJECTS: '/projects',
+   TECHS: '/techs',
+   EXPERIENCE: '/experience',
+}
+
+Object.freeze(ROUTES)
+
+export const BASE_PATH = '/portfolio'
